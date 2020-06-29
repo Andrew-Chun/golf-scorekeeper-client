592696146f3a9f4d923dbabada56a76ee50f8297
Andrew-Chun <i.am.andrewchun@gmail.com>
2020-06-29 11:53:02 -0500

    Show/hide user auth forms at appropriate times

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,38 +1,46 @@
 const store = require('./../store')
 
 const signUpSuccess = responseData => {
-  $('sign-up').trigger('reset')
+  $('.sign-up').trigger('reset')
   $('#message').text(`Signed up successfully!`).removeClass().addClass('success')
 }
 
 const signUpFailure = function () {
-  $('sign-up').trigger('reset')
+  $('.sign-up').trigger('reset')
   $('#message').text('Sign up failure.').removeClass().addClass('failure')
 }
 
 const signInSuccess = responseData => {
-  $('form').trigger('reset')
+  $('.sign-in').trigger('reset')
   $('#message').text(`${responseData.user.email} signed in successfully!`).removeClass().addClass('success').show()
+  $('.sign-up').hide()
+  $('.sign-in').hide()
+  $('.change-pw').show()
+  $('.sign-out').show()
   store.user = responseData.user
 }
 
 const signInFailure = () => {
-  $('form').trigger('reset')
+  $('.sign-in').trigger('reset')
   $('#message').text('Sign in failure.').removeClass().addClass('failure')
 }
 
 const changePwSuccess = () => {
-  $('form').trigger('reset')
+  $('.change-pw').trigger('reset')
   $('#message').text(`Password changed successfully!`).removeClass().addClass('success').show()
 }
 
 const changePwFailure = () => {
-  $('form').trigger('reset')
+  $('.change-pw').trigger('reset')
   $('#message').text('Password change failure.').removeClass().addClass('failure').show()
 }
 
 const signOutSuccess = () => {
   $('#message').text(`Signed Out successfully!`).removeClass().addClass('success').show()
+  $('.sign-up').show()
+  $('.sign-in').show()
+  $('.change-pw').hide()
+  $('.sign-out').hide()
 }
 
 const signOutFailure = () => {
