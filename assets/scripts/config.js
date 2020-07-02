cdad48359474b355655c6109ea7abcc7f2d09446
Andrew-Chun <i.am.andrewchun@gmail.com>
2020-07-02 12:55:52 -0500

    removed trailing "/" for deployment heroku url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://golf-scorekeeper-api.herokuapp.com/',
+  production: 'https://golf-scorekeeper-api.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
